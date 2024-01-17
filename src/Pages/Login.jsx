274c5ddf87840c106fe006fb67e8454aa74e1661
rohit shyam mane <rohitmane8356@gmail.com>
2024-01-17 17:55:49 +0530

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -5,11 +5,22 @@ import { UserContext } from "../Context/UserContext"
 
 
 function login() {
-  const [email,setemail]= useState("")
-  const [password,setpassword]=useState("")
-  const [redirect,setredirect]= useState(false)
-const {setuser} = useContext(UserContext)
-  
+  const [email,setemail]= useState("");
+  const [password,setpassword]=useState("");
+  const [redirect,setredirect]= useState(false);
+const {setuser} = useContext(UserContext);
+
+
+const handleAsGuest=async(e)=>{
+  e.preventDefault()
+  setemail("rohit@gmail.com")
+  setpassword("rohit")
+  const {data}= await axios.post("/login",{email,password})
+  setuser(data)
+  alert("Login Successfully")
+       setredirect(true)
+}
+
   const LoginUser=async(e)=>{
    
     e.preventDefault()
@@ -54,6 +65,7 @@ const {setuser} = useContext(UserContext)
       <input type="text" placeholder="your@email.com" value={email} onChange={e=> setemail(e.target.value)}/>
       <input type="password" placeholder="password" value={password} onChange={e=> setpassword(e.target.value)}/>
       <button className="primary" type="submit">login</button>
+      <button className="primary mt-1" onClick={handleAsGuest}>Login as Guest</button>
       <div className="text-center py-2 text-gray-500">Don`t have account yet? <Link to={"/register"} className="underline text-black">Register now</Link>
       </div>
      </form>
