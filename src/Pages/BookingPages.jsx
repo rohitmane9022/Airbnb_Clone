cfad15b36dcbf6bb2fc938219984b42454388974
rohit shyam mane <rohitmane8356@gmail.com>
2024-03-15 17:06:08 +0530

    intial changes

--- src/Pages/BookingPages.jsx
+++ src/Pages/BookingPages.jsx
@@ -22,11 +22,11 @@ export default function BookingPages() {
         {
           booking?.length > 0 && booking?.map(booking=>(
             <Link to={"/account/booking/"+booking._id} key={booking._id} className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden my-5">
-              <div className="w-48">
+              <div className="w-auto">
                 <PlaceImg place={booking.place}/>
               </div>
               <div className="py-4 pr-3 grow">
-                <h2 className="text-xl">{booking.place.title}</h2>
+                <h2 className="text-base font-bold sm:text-xl sm:font-normal">{booking.place.title}</h2>
                 <div className="text-xl">
                   <div className="flex gap-1 mb-2 mt-2 text-sm text-gray-500">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
@@ -51,10 +51,10 @@ export default function BookingPages() {
  </div>
 
 <div className="flex gap-1 items-center">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 sm:w-8 sm:h-8">
   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
 </svg>
-<span className="text-lg">Total Price: ₹{booking.price}</span>
+<span className="text-sm sm:text-lg">Total Price: ₹{booking.price}</span>
 </div>
        </div>
              
--- src/Pages/BookingSinglePages.jsx
+++ src/Pages/BookingSinglePages.jsx
@@ -26,15 +26,15 @@ export default function BookingSinglePages() {
 
   return (
     <div className='my-8'>
-      <h1 className='text-3xl'>{booking.place.title}</h1>
-      <a className="my-2 block font-semibold underline flex gap-1" href={"https://maps.google.com/?q="+booking.place.address} target="blank" ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+      <h1 className='text-xl sm:text-3xl'>{booking.place.title}</h1>
+      <a className="my-2 block text-sm sm:text-base font-semibold underline flex gap-1" href={"https://maps.google.com/?q="+booking.place.address} target="blank" ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 sm:w-6 sm:h-6">
   <path fillRule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clipRule="evenodd" />
 </svg>
 {booking.place.address}
 </a>
-<div className='bg-gray-200 p-6 my-6 rounded-2xl flex justify-between items-center'>
+<div className='bg-gray-200 p-6 my-6 rounded-2xl  flex flex-col  sm:flex-row sm:justify-between sm:items-center'>
   <div>
-  <h2 className='text-2xl mb-4'>Your Booking Information:</h2>
+  <h2 className='text-xl sm:text-2xl mb-4'>Your Booking Information:</h2>
   <div className="flex gap-1 mb-2 mt-2 text-sm text-gray-500">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
   <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
@@ -58,9 +58,9 @@ export default function BookingSinglePages() {
  </div>
   </div>
  
- <div className='bg-primary p-4 text-white rounded-2xl flex flex-col justify-center items-center'>
- <div>Total Price</div>
- <div className='text-3xl'>₹{booking.price}</div>
+ <div className='bg-primary p-4 text-white rounded-2xl flex sm:flex-col gap-1 justify-center items-center mt-4 sm:mt-auto'>
+ <div className='text-xl'>Total Price </div>
+ <div className='text-xl sm:text-3xl'>₹{booking.price}</div>
 </div>
 </div>
 <PlaceGallary Place={booking.place}/>
